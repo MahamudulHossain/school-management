f6fff2db5c21e369707a0bd669b6902e18f8bfbe
MahamudulHossain <md.mahamudulhossainn@gmail.com>
2026-06-01 19:15:42 +0600

    Default config added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'EISL') }} </title>
+    <title>{{ config('app.name', 'Hossainn') }} </title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css"
@@ -63,7 +63,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'EISL') }}
+                {{ config('app.name', 'Hossainn') }}
             </a>
         </div>
 
